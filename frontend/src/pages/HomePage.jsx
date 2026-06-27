06fdf9092847985b90e5802d0e471ba7afcd2fad
Mohammed Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2026-06-27 23:37:13 +0600

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import HeroSection from '../components/Home/HeroSection';
-import FeaturesSection from '../components/Home/FeaturesSection';
-import HowItWorks from '../components/Home/HowItWorks';
-import StatsSection from '../components/Home/StatsSection';
-import CTASection from '../components/Home/CTASection';
-import Footer from '../components/Home/Footer';
+import { Navbar, HeroSection, FeaturesSection, HowItWorks, StatsSection, CTASection, Footer } from '../components/Home/HomeComponents';
 import '../styles/home.css';
 
 const HomePage = () => {
     return (
         <main className="home-page">
+            <Navbar />
             <HeroSection />
             <FeaturesSection />
             <HowItWorks />
